e1a5cedde2cc86600bf26a5f1f037fc95c81a47d
Headphone_Sang <1527569640@qq.com>
2026-03-23 07:52:05 +0800

    消融实验:spec_loss

--- run.py
+++ run.py
@@ -26,9 +26,9 @@ parameter_dict = {
     'valid_metric': 'NDCG@10',
     'moe_gate_t': 0.5,
     'align_lambda': 0.1,
-    'spec_lambda': -0.1,
+    'spec_lambda': -0.01,
     'bal_lambda': 0.1,
-    'time_b': 1.5,
+    'time_b': 1.5,  # The larger the time_b, the more the impact of time on the differences among the samples.
     'kernel_mul': 2,
     'kernel_num': 2,  # The more kernel there are, the greater the impact of MMD on the differences among the samples.
     'conv_kernel_size': 3
